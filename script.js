faa7ab24351a3faa170cdebc59a791442a6a98d6
barbara-musial <basia.musial22@gmail.com>
2022-03-21 17:26:33 +0100

    Create function that sets current temperature

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ const weatherIconBox = document.querySelector(".weather--icon--box");
 const weatherDescriptionBox = document.querySelector(
   ".weather--description--box"
 );
+const currentTempBox = document.querySelector(".current--temperature--box");
 
 const getData = function (coords) {
   const [lat, lon] = coords;
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=2036b1729952c5742fea723833b9919b`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=2036b1729952c5742fea723833b9919b&units=metric`
   )
     .then((response) => response.json())
     .then((data) => {
@@ -16,9 +17,11 @@ const getData = function (coords) {
 
       const weatherIcon = data.weather[0].icon;
       const weatherDescription = data.weather[0].description;
+      const weatherCurrentTemp = data.main.temp;
 
       displayWeatherIcon(weatherIcon);
       setWeatherDescription(weatherDescription);
+      setCurrentTemp(weatherCurrentTemp);
     });
 };
 getData([52.409538, 16.931992]);
@@ -34,4 +37,8 @@ function displayWeatherIcon(icon) {
 
 function setWeatherDescription(description) {
   weatherDescriptionBox.textContent = description;
+}
+
+function setCurrentTemp(temp) {
+  currentTempBox.textContent = `${Number(temp).toFixed(0)}℃`;
 }
