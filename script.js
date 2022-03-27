ad7e87d8a38d5c09424af0760f9dc8bc53848c7e
barbara-musial <basia.musial22@gmail.com>
2022-03-27 20:59:35 +0200

    Delete functions and import them as modules

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-"use strict";
+import { convertTimestampToTime } from "./modules/convertTimestamp.js";
+import {
+  displayImg,
+  displayData,
+  displayCurrDateAndTime,
+} from "./modules/display.js";
+import { getDataFromAPI as getData } from "./modules/getDataFromAPI.js";
 
 // Containers
 const timeBackgroundImg = document.querySelector(".time-img");
@@ -18,125 +24,99 @@ const windDirectCont = document.querySelector(".wind-direction");
 const humidityCont = document.querySelector(".humidity");
 const pressureCont = document.querySelector(".pressure");
 
-const getAndShowData = function (coords) {
+async function displayWeatherData(coords) {
   const [lat, lon] = coords;
 
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=2036b1729952c5742fea723833b9919b&units=metric`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      const currTemp = `${Number(data.main.temp).toFixed(0)}°C`;
-      const feelsLikeCurrTemp = `${Number(data.main.feels_like).toFixed(0)}°C`;
-      const weatherIcon = data.weather[0].icon;
-      const weatherDescription = data.weather[0].description;
-      const location = data.name;
-      const timezone = data.timezone;
-      const sunriseTime = convertTimestampToTime(data.sys.sunrise + timezone);
-      const sunsetTime = convertTimestampToTime(data.sys.sunset + timezone);
-      const minTemp = `${Number(data.main.temp_min).toFixed(0)}°C`;
-      const maxTemp = `${Number(data.main.temp_max).toFixed(0)}°C`;
-      const windSpeed = `${(Number(data.wind.speed) * 3.6).toFixed(0)} km/h`;
-      const windDegrees = data.wind.deg;
-      let windDirect;
-      const humidity = `${data.main.humidity}%`;
-      const pressure = `${data.main.pressure} hPa`;
+  const weatherData = await getData(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts&units=metric&appid=2036b1729952c5742fea723833b9919b`
+  );
 
-      // Set wind direction
-      if (windDegrees >= 349 || windDegrees <= 11) {
-        windDirect = "N";
-      } else if (windDegrees >= 12 && windDegrees <= 33) {
-        windDirect = "NNE";
-      } else if (windDegrees >= 34 && windDegrees <= 56) {
-        windDirect = "NE";
-      } else if (windDegrees >= 57 && windDegrees <= 78) {
-        windDirect = "ENE";
-      } else if (windDegrees >= 79 && windDegrees <= 101) {
-        windDirect = "E";
-      } else if (windDegrees >= 102 && windDegrees <= 123) {
-        windDirect = "ESE";
-      } else if (windDegrees >= 124 && windDegrees <= 146) {
-        windDirect = "SE";
-      } else if (windDegrees >= 147 && windDegrees <= 168) {
-        windDirect = "SSE";
-      } else if (windDegrees >= 169 && windDegrees <= 191) {
-        windDirect = "S";
-      } else if (windDegrees >= 192 && windDegrees <= 213) {
-        windDirect = "SSW";
-      } else if (windDegrees >= 214 && windDegrees <= 236) {
-        windDirect = "SW";
-      } else if (windDegrees >= 237 && windDegrees <= 258) {
-        windDirect = "WSW";
-      } else if (windDegrees >= 259 && windDegrees <= 281) {
-        windDirect = "W";
-      } else if (windDegrees >= 282 && windDegrees <= 303) {
-        windDirect = "WNW";
-      } else if (windDegrees >= 304 && windDegrees <= 326) {
-        windDirect = "NW";
-      } else if (windDegrees >= 327 && windDegrees <= 348) {
-        windDirect = "NNW";
-      }
+  const location = (
+    await getData(
+      `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lon}&apiKey=f52a5f9fe97247faaeb2a726f9ca5405`
+    )
+  ).features[0].properties.city;
+  const currTemp = `${weatherData.current.temp.toFixed(0)}°C`;
+  const feelsLikeCurrTemp = `${weatherData.current.feels_like.toFixed(0)}°C`;
+  const currWeatherIcon = weatherData.current.weather[0].icon;
+  const currWeatherDescription = weatherData.current.weather[0].description;
+  const timezoneOffset = weatherData.timezone_offset;
+  const sunriseTime = convertTimestampToTime(
+    weatherData.current.sunrise + timezoneOffset
+  );
+  const sunsetTime = convertTimestampToTime(
+    weatherData.current.sunset + timezoneOffset
+  );
+  const minTemp = `${weatherData.daily[0].temp.min.toFixed(0)}°C`;
+  const maxTemp = `${weatherData.daily[0].temp.max.toFixed(0)}°C`;
+  const windSpeed = `${(weatherData.current.wind_speed * 3.6).toFixed(0)}km/h`;
+  const windDegrees = weatherData.current.wind_deg;
+  let windDirect;
+  const humidity = `${weatherData.current.humidity}%`;
+  const pressure = `${weatherData.current.pressure} hPa`;
 
-      // Display background time img
-      displayImg(
-        weatherIcon.charAt(weatherIcon.length - 1) === "n" ? "night" : "day",
-        timeBackgroundImg,
-        "jpg"
-      );
+  // Set wind direction
+  if (windDegrees >= 349 || windDegrees <= 11) {
+    windDirect = "N";
+  } else if (windDegrees >= 12 && windDegrees <= 33) {
+    windDirect = "NNE";
+  } else if (windDegrees >= 34 && windDegrees <= 56) {
+    windDirect = "NE";
+  } else if (windDegrees >= 57 && windDegrees <= 78) {
+    windDirect = "ENE";
+  } else if (windDegrees >= 79 && windDegrees <= 101) {
+    windDirect = "E";
+  } else if (windDegrees >= 102 && windDegrees <= 123) {
+    windDirect = "ESE";
+  } else if (windDegrees >= 124 && windDegrees <= 146) {
+    windDirect = "SE";
+  } else if (windDegrees >= 147 && windDegrees <= 168) {
+    windDirect = "SSE";
+  } else if (windDegrees >= 169 && windDegrees <= 191) {
+    windDirect = "S";
+  } else if (windDegrees >= 192 && windDegrees <= 213) {
+    windDirect = "SSW";
+  } else if (windDegrees >= 214 && windDegrees <= 236) {
+    windDirect = "SW";
+  } else if (windDegrees >= 237 && windDegrees <= 258) {
+    windDirect = "WSW";
+  } else if (windDegrees >= 259 && windDegrees <= 281) {
+    windDirect = "W";
+  } else if (windDegrees >= 282 && windDegrees <= 303) {
+    windDirect = "WNW";
+  } else if (windDegrees >= 304 && windDegrees <= 326) {
+    windDirect = "NW";
+  } else if (windDegrees >= 327 && windDegrees <= 348) {
+    windDirect = "NNW";
+  }
 
-      // Display weather Icon
-      weatherIconCont.classList.add(
-        `icon-${
-          weatherIcon.charAt(weatherIcon.length - 1) === "n" ? "night" : "day"
-        }`
-      );
-      displayImg(weatherIcon, weatherIconCont, "png");
+  // Display background time img
+  displayImg(
+    currWeatherIcon.at(-1) === "n" ? "night" : "day",
+    timeBackgroundImg,
+    "jpg"
+  );
 
-      // Display rest of data
-      displayData(currTemp, currTempCont);
-      displayData(feelsLikeCurrTemp, feelsLikeCont);
-      displayData(weatherDescription, weatherDescCont);
-      displayData(location, locationCont);
-      displayData(sunriseTime, sunriseTimeCont);
-      displayData(sunsetTime, sunsetTimeCont);
-      displayData(minTemp, minTempCont);
-      displayData(maxTemp, maxTempCont);
-      displayData(windSpeed, windSpeedCont);
-      displayData(windDirect, windDirectCont);
-      displayData(humidity, humidityCont);
-      displayData(pressure, pressureCont);
+  // Display weather Icon
+  weatherIconCont.classList.add(
+    `icon-${currWeatherIcon.at(-1) === "n" ? "night" : "day"}`
+  );
+  displayImg(currWeatherIcon, weatherIconCont, "png");
 
-      displayCurrDateAndTime();
-    });
-};
-getAndShowData([52.409538, 16.931992]);
+  // Display rest of data
+  displayData(currTemp, currTempCont);
+  displayData(feelsLikeCurrTemp, feelsLikeCont);
+  displayData(currWeatherDescription, weatherDescCont);
+  displayData(location, locationCont);
+  displayData(sunriseTime, sunriseTimeCont);
+  displayData(sunsetTime, sunsetTimeCont);
+  displayData(minTemp, minTempCont);
+  displayData(maxTemp, maxTempCont);
+  displayData(windSpeed, windSpeedCont);
+  displayData(windDirect, windDirectCont);
+  displayData(humidity, humidityCont);
+  displayData(pressure, pressureCont);
 
-function displayData(data, container) {
-  container.textContent = data;
+  displayCurrDateAndTime(currDayCont, currTimeCont);
 }
-
-function displayImg(img, container, format) {
-  container.src = `./images/${img}.${format}`;
-}
-
-function convertTimestampToTime(timestamp) {
-  const date = new Date(timestamp * 1000);
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-
-  return `${hours}:${minutes <= 9 ? "0" + minutes : minutes}`;
-}
-function convertTimestampToDate(timestamp) {
-  const date = new Date(timestamp * 1000);
-  const weekDay = date.toLocaleDateString("en-US", { weekday: "long" });
-  const day = date.toLocaleDateString("en-US", { day: "numeric" });
-  const month = date.toLocaleDateString("en-US", { month: "long" });
-
-  return `${weekDay}, ${day} ${month} `;
-}
-
-function displayCurrDateAndTime() {
-  const currDateTimestamp = Math.floor(new Date(new Date()).getTime() / 1000);
-  currDayCont.textContent = convertTimestampToDate(currDateTimestamp);
-  currTimeCont.textContent = convertTimestampToTime(currDateTimestamp);
-}
+displayWeatherData([52.71051, 16.38044]);
