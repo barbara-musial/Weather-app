76e199590291d5a48b08c2ff589925ff4879f468
barbara-musial <basia.musial22@gmail.com>
2022-03-31 21:00:02 +0200

    Handle fetching getting data errors

--- modules/getDataFromAPI.js
+++ modules/getDataFromAPI.js
@@ -1,7 +1,11 @@
 export async function getDataFromAPI(endpointURL) {
   const data = await fetch(endpointURL)
     .then((response) => response.json())
-    .then((data) => data);
+    .then((data) => data)
+    .catch((error) => {
+      console.error(`Something went wrong! ${error}`);
+      return "Something went wrong... Try again!";
+    });
 
   return data;
 }
