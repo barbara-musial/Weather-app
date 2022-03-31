44a8bdd5c30eb83b28589401ce017e072ea029fc
barbara-musial <basia.musial22@gmail.com>
2022-03-31 20:58:53 +0200

    Create condition when unknown city name is searched

--- modules/getCityNameCoords.js
+++ modules/getCityNameCoords.js
@@ -4,10 +4,17 @@ export async function getCityNameCoords(cityName) {
   const data = await getDataFromAPI(
     `https://api.geoapify.com/v1/geocode/search?text=${cityName}&lang=en&limit=10&type=city&apiKey=f52a5f9fe97247faaeb2a726f9ca5405`
   );
-  const coords = [
-    data.features[0].properties.lat,
-    data.features[0].properties.lon,
-  ];
 
-  return coords;
+  if (data.features.length === 0) {
+    console.error("Unknown city, try again!");
+
+    return "error";
+  } else {
+    const coords = [
+      data.features[0].properties.lat,
+      data.features[0].properties.lon,
+    ];
+
+    return coords;
+  }
 }
